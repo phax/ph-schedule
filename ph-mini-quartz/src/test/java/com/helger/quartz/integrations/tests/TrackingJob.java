ba2e5af4c98120041674a5f539e3474edd4f3aab
phax <philip@helger.com>
2016-07-22 13:10:22 +0200

    Version update

--- ph-mini-quartz/src/test/java/com/helger/quartz/integrations/tests/TrackingJob.java
+++ ph-mini-quartz/src/test/java/com/helger/quartz/integrations/tests/TrackingJob.java
@@ -1,11 +1,14 @@
-/*
- * Copyright 2013 Terracotta, Inc..
+/**
+ * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
+ *
+ * Copyright (C) 2016 Philip Helger (www.helger.com)
+ * philip[at]helger[dot]com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
