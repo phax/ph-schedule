6eadbc3ece9f7ee52ebc2dcb2a5ea7b50fe28aa4
Philip Helger <philip@helger.com>
2025-01-21 11:16:49 +0100

    Updated license headers to 2025

--- ph-mini-quartz/src/main/java/com/helger/quartz/utils/counter/sampled/SampledRateCounterConfig.java
+++ ph-mini-quartz/src/main/java/com/helger/quartz/utils/counter/sampled/SampledRateCounterConfig.java
@@ -1,7 +1,7 @@
 /*
  * All content copyright Terracotta, Inc., unless otherwise indicated. All rights reserved.
  *
- * Copyright (C) 2016-2024 Philip Helger (www.helger.com)
+ * Copyright (C) 2016-2025 Philip Helger (www.helger.com)
  * philip[at]helger[dot]com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
